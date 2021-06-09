4727982afc96ae55e1ec5c58a493426a653d703b
Floyd May <floyd@canyon-trail.com>
2021-06-09 16:51:33 -0500

    initial POC for shapes

---
+++ Shapes.POC/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.IO;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
+
+namespace Shapes.POC
+{
+    public sealed class Program
+    {
+        public static void Main()
+        {
+            var root = Path.Combine(Directory.GetCurrentDirectory(), "Generated");
+
+            var ns = NamespaceDeclaration(ParseName("Shapes.POC.Generated"));
+
+            using var writer = new StreamWriter(Path.Combine(root, "Shapes.Generated.cs"), false);
+
+            var triangleClasses = Shapes.Triangle.GetValidPermutations()
+                .Select(x => CreateClass(Shapes.Triangle, x));
+
+            ns.AddMembers(triangleClasses.ToArray())
+                .NormalizeWhitespace()
+                .WriteTo(writer);
+        }
+
+        private static ClassDeclarationSyntax CreateClass(Shape shape, ImmutableArray<Feature> enabledFeatures)
+        {
+            var featureSuffix = string.Join("_", enabledFeatures.Select(x => x.Name));
+
+            var className = $"{shape.Name}_{featureSuffix}".TrimEnd('_');
+
+            var fields = enabledFeatures.SelectMany(x => x.Parameters)
+                .Select(x =>
+                    VariableDeclaration(IdentifierName(x.Value.FullName))
+                        .WithVariables(SingletonSeparatedList(VariableDeclarator(Identifier(x.Key)))))
+                .Select(x => FieldDeclaration(x).AddModifiers(Token(SyntaxKind.PrivateKeyword)));
+
+            var klass = ClassDeclaration(Identifier(className))
+                .AddModifiers(Token(SyntaxKind.PublicKeyword))
+                .AddMembers(fields.ToArray());
+
+            if (shape.CanDraw(enabledFeatures))
+            {
+                klass = klass.AddMembers(
+                    MethodDeclaration(PredefinedType(Token(SyntaxKind.VoidKeyword)), Identifier("Draw"))
+                        .AddModifiers(Token(SyntaxKind.PublicKeyword))
+                        .WithBody(Block()));
+            }
+            return klass;
+        }
+    }
+
+    public sealed class Features
+    {
+        public static readonly Feature StrokeColor = new Feature("StrokeColor", new Dictionary<string, Type> {
+                ["strokeColor"] = typeof(uint),
+            });
+        public static readonly Feature FillColor = new Feature("FillColor", new Dictionary<string, Type> {
+                ["fillColor"] = typeof(uint),
+            });
+        public static readonly Feature CornerRadius = new Feature("CornerRadius", new Dictionary<string, Type> {
+                ["cornerRadius"] = typeof(float),
+            });
+    }
+
+    public sealed class Shapes
+    {
+        public static readonly Shape Triangle = new Shape("Triangle", typeof(Triangle), ImmutableArray.Create(new[]
+        {
+            ImmutableArray.Create(Features.StrokeColor),
+            ImmutableArray.Create(Features.StrokeColor, Features.FillColor),
+            ImmutableArray.Create(Features.StrokeColor, Features.FillColor, Features.CornerRadius),
+        }));
+    }
+
+    public static class Triangle
+    {
+        public static void Draw(uint strokeColor) { }
+        public static void Draw(uint strokeColor, uint fillColor) { }
+        public static void Draw(uint strokeColor, uint fillColor, float cornerRadius) { }
+    }
+}
---
+++ Shapes.POC/Shape.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+
+namespace Shapes.POC
+{
+    public sealed class Shape
+    {
+        public string Name { get; }
+        public Type ImplementationType { get; }
+        public ImmutableArray<ImmutableArray<Feature>> ValidFeatureCombinations { get; }
+        public ImmutableSortedSet<string> SupportedFeatureNames =>
+            ValidFeatureCombinations
+                .SelectMany(x => x)
+                .Select(x => x.Name)
+                .ToImmutableSortedSet();
+        public ImmutableArray<Feature> Features =>
+            ValidFeatureCombinations
+                .SelectMany(x => x)
+                .GroupBy(x => x.Name)
+                .Select(x => x.First())
+                .ToImmutableArray();
+
+        public Shape(string name, Type implementationType, ImmutableArray<ImmutableArray<Feature>> validFeatureCombinations)
+        {
+            Name = name;
+            ImplementationType = implementationType;
+            ValidFeatureCombinations = validFeatureCombinations;
+        }
+
+        public IEnumerable<ImmutableArray<Feature>> GetValidPermutations()
+        {
+            return PermuteFeatures(SupportedFeatureNames.ToImmutableArray())
+                .Where(CanLeadToValid)
+                .Select(x => x.Select(GetFeatureByName).ToImmutableArray());
+        }
+
+        public Feature GetFeatureByName(string name)
+        {
+            return Features.Single(x => x.Name == name);
+        }
+
+        public bool CanDraw(ImmutableArray<Feature> enabledFeatures)
+        {
+            var featureNames = enabledFeatures.Select(x => x.Name).ToArray();
+            return ValidFeatureCombinations
+                .Select(x => x.Select(f => f.Name).ToImmutableHashSet())
+                .Any(set => set.SetEquals(featureNames));
+        }
+
+        private bool CanLeadToValid(ImmutableSortedSet<string> candidate)
+        {
+            return ValidFeatureCombinations.Any(x => candidate.IsSubsetOf(x.Select(f => f.Name)));
+        }
+
+        private IEnumerable<ImmutableSortedSet<string>> PermuteFeatures(ImmutableArray<string> features)
+        {
+            if(features.IsEmpty)
+                return new[] { ImmutableSortedSet<string>.Empty };
+
+            var first = features[0];
+
+            var rest = features.Skip(1).ToImmutableArray();
+
+            return PermuteFeatures(rest)
+                .SelectMany(x => new[] { x, x.Add(first) })
+                .ToArray();
+        }
+    }
+
+    public sealed class Feature
+    {
+        public string Name { get; }
+        public IDictionary<string, Type> Parameters { get; }
+
+        public Feature(string name, IDictionary<string, Type> parameters)
+        {
+            Name = name;
+            Parameters = parameters;
+        }
+    }
+
+}
